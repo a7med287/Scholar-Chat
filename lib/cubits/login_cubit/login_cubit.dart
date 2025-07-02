aa306cf9fcb14561ae90830512d55a7e18bd89f3
Ahmed Rizk <rezk18649@gmail.com>
2025-07-02 17:32:43 +0300

    add login cubit

--- lib/cubits/login_cubit/login_cubit.dart
+++ lib/cubits/login_cubit/login_cubit.dart
@@ -1,9 +1,24 @@
-
-
 import 'package:chat_scholar/cubits/login_cubit/login_states.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginCubit extends Cubit{
+class LoginCubit extends Cubit {
+  LoginCubit() : super(LoginInitial());
 
-  LoginCubit(): super(LoginInitial());
+
+  Future<void> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+   try{
+     await FirebaseAuth.instance.signInWithEmailAndPassword(
+       email: email,
+       password: password,
+     );
+     emit(LoginSuccess());
+   }catch(e){
+     emit(LoginFailure());
+   }
+  }
 }
